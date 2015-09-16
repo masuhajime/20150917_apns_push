6c96d9fc4b1745b9505f65e67d2f47963773ebba
masuhajime <masuhajime@masuhajime.local>
2015-09-17 02:43:21 +0900

    1st commit

---
+++ ApnsMessageSender.php
@@ -0,0 +1,84 @@
+<?php
+// https://github.com/immobiliare/ApnsPHP
+require_once dirname(__FILE__).'/ApnsPHP/ApnsPHP/Autoload.php';
+
+class ApnsMessageSender
+{
+    private $pushSocket = null;
+
+    private $pushMessage = null;
+
+    private $deviceTokens = array();
+
+    /**
+     * @param string $environment ApnsPHP_Abstract::{ENVIRONMENT_SANDBOX, ENVIRONMENT_PRODUCTION}
+     * @param filepath $providerCertificateFile
+     * @param filepath $rootCertificationAuthority
+     */
+    public function __construct($environment, $providerCertificateFile, $rootCertificationAuthority)
+    {
+        $this->pushSocket = new ApnsPHP_Push($environment, $providerCertificateFile);
+        $this->pushSocket->setRootCertificationAuthority($rootCertificationAuthority);
+        $this->pushSocket->connect();
+    }
+
+    /**
+     * @param array $tokens デバイストークン, 文字列の配列
+     * @return $this
+     */
+    public function setDeviceTokens(array $tokens)
+    {
+        $this->deviceTokens = $tokens;
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return $this
+     */
+    public function setMessage($message)
+    {
+        $this->pushMessage = $message;
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function send()
+    {
+        if (empty($this->pushMessage)) {
+            throw new RuntimeException("push message is empty");
+        }
+        if (0 === count($this->deviceTokens)) {
+            return;
+        }
+        foreach ($this->deviceTokens as $token) {
+            $this->pushSocket->add(
+                $this->createMessage($token, $this->pushMessage)
+            );
+        }
+        $this->pushSocket->send();
+    }
+
+    /**
+     * @param $deviceToken
+     * @param $pushMessage
+     * @return ApnsPHP_Message
+     */
+    private function createMessage($deviceToken, $pushMessage)
+    {
+        $message = new ApnsPHP_Message($deviceToken);
+        $message->setText($pushMessage);
+        $message->setExpiry(30);
+        return $message;
+    }
+
+    function __destruct()
+    {
+        if (is_null($this->pushSocket)) {
+            return;
+        }
+        $this->pushSocket->disconnect();
+    }
+}
---
+++ sample_push.php
@@ -0,0 +1,24 @@
+<?php
+require_once dirname(__FILE__)."/ApnsMessageSender.php";
+
+date_default_timezone_set('Asia/Tokyo');
+
+try {
+    $sender = new ApnsMessageSender(
+        ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
+        dirname(__FILE__)."/server_certificates_sandbox.pem",
+        dirname(__FILE__)."/entrust_root_certification_authority.pem"
+    );
+    // 指定の全端末に同じメッセージをpush
+    $sender
+        ->setMessage(sprintf("message date:%s", date("Y-m-d H:i:s")))
+        ->setDeviceTokens(
+            array(
+                "<デバイストークン>",
+            )
+        )
+        ->send();
+} catch (Exception $e) {
+    // なんらかの例外処理
+    echo $e->getMessage();
+}
